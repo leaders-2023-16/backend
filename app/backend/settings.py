254e6bc6e5f4ecd43509f0a00f30b92ec2a20711
Andrew Klyuchnikov <off-rey@bk.ru>
2023-05-16 23:12:53 +0300

    Feature/static (#4)
    * Add static root to settings and improve Dockerfile
    
    ---------
    
    Co-authored-by: Andrey Klychnikov <off-rey@bk.com>

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
