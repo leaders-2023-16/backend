eff5ed370dff821c2e0c1b3328e40abdbb706215
Andrey Klychnikov <off-rey@bk.com>
2023-05-19 01:45:04 +0300

    :truck: rename countries API route from /users/countries to /countries

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r"trainee-profiles", TraineeProfileViewSet, basename="trainee-profiles")
+router.register(
+    r"users/trainee-profiles", TraineeProfileViewSet, basename="trainee-profiles"
+)
 router.register(r"countries", CountryViewSet, basename="countries")
 
 urlpatterns = [
