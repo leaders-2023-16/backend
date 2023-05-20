cd4514aa8865017f87edbbe23334c705e996f46c
Andrey Klychnikov <off-rey@bk.com>
2023-05-21 01:13:10 +0300

    :construction: [WIP] Add vacancies API

--- app/internship/views.py
+++ app/internship/views.py
@@ -1,9 +1,11 @@
-from accounts.permissions import IsCandidate, IsCurator
+from accounts.permissions import IsCandidate, IsCurator, IsPersonnel
 from django_filters.rest_framework import DjangoFilterBackend
-from internship.models import InternshipApplication
+from internship.models import InternshipApplication, Vacancy
 from internship.serializers import (
     InternshipApplicationSerializer,
     ReadInternshipApplicationSerializer,
+    ReadVacancySerializer,
+    VacancySerializer,
 )
 from rest_framework import permissions, viewsets
 
@@ -24,4 +26,23 @@ class InternshipApplicationViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action not in ("update", "partial_update", "create"):
             return ReadInternshipApplicationSerializer
+        return self.serializer_class
+
+
+class VacancyViewSet(viewsets.ModelViewSet):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["direction"]
+
+    def get_permissions(self):
+        if self.action == "create" or self.action == "destroy":
+            return [permissions.IsAuthenticated(), IsPersonnel()]
+        elif self.action == "update" or self.action == "partial_update":
+            return [permissions.IsAuthenticated(), IsCurator() | IsPersonnel]
+        return [permissions.IsAuthenticated()]
+
+    def get_serializer_class(self):
+        if self.action not in ("update", "partial_update", "create"):
+            return ReadVacancySerializer
         return self.serializer_class
